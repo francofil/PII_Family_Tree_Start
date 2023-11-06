53229a9d5371a61c2095c871124fc77ef8964ed6
maruuroman <maria.roman@correo.ucu.edu.uy>
2023-11-05 22:39:01 -0300

    clase visitor, cambios en el program y detalles

--- src/Library/Visitor.cs
+++ src/Library/Visitor.cs
@@ -4,8 +4,30 @@ using System;
 
 namespace Library
 {
-    public class Visitor
+    public class AgeVisitor
     {
+        public int TotalAge { get; private set; }
+
+        public AgeVisitor()
+        {
+            TotalAge = 0; 
+        }
+
+
+        public void Visit(Node node)
+        {
+            // Verifica si el nodo tiene una Person
+            if (node.Person != null)
+            {
+                TotalAge += node.Person.Age;
+            }
+
+            // Recorre los hijos del nodo 
+            foreach (var child in node.Children)
+            {
+                Visit(child);   // para asegurarse de que se visiten todos los nodos del árbol.
+            }
+        }
 
     }
 }
